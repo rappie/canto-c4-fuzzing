5055292eff60bb2c71ccaee3ee3864043d5d9c16
Rappie <jasper@rappie.com>
2023-03-18 22:13:38 +0100

    Buy helper

--- src/echidna/EchidnaHelper.sol
+++ src/echidna/EchidnaHelper.sol
@@ -2,4 +2,12 @@
 
 import "./EchidnaSetup.sol";
 
-contract EchidnaHelper is EchidnaSetup {}
+contract EchidnaHelper is EchidnaSetup {
+
+    function buy(uint8 fromAccId, uint256 amount) public {
+        address from = getAccountFromUint8(fromAccId);
+
+        hevm.prank(from);
+        tray.buy(amount);
+    }
+}
