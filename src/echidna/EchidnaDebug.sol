551741afd59df56129a6f99bd20fe1b225e9a809
Rappie <jasper@rappie.com>
2023-03-19 16:06:10 +0100

    Debugging

--- src/echidna/EchidnaDebug.sol
+++ src/echidna/EchidnaDebug.sol
@@ -8,28 +8,4 @@ contract EchidnaDebug is EchidnaHelper {
 
     bool debug = false;
 
-    function debugNamespace() public {
-        require(debug);
-
-        address from = ADDRESS_DEPLOYER;
-
-        uint256 trayId = tray.nextTokenId();
-        hevm.prank(from);
-        tray.buy(1);
-
-        Namespace.CharacterData[] memory list = new Namespace.CharacterData[](
-            1
-        );
-        list[0] = Namespace.CharacterData(trayId, 0, 0);
-
-        hevm.prank(from);
-        try namespace.fuse(list) {
-            Debugger.log("success");
-        } catch {
-            Debugger.log("fail");
-        }
-
-        assert(false);
-    }
-
 }
