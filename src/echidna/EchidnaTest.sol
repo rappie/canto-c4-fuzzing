9eb1b01265c170a46e3957e32f60d4a49f937feb
Rappie <jasper@rappie.com>
2023-03-19 12:11:20 +0100

    testTokenURIShouldNotRevert

--- src/echidna/EchidnaTest.sol
+++ src/echidna/EchidnaTest.sol
@@ -6,4 +6,19 @@ import "./EchidnaDebug.sol";
 import "./Debugger.sol";
 import "./Tools.sol";
 
-contract EchidnaTest is EchidnaSetup, EchidnaHelper, EchidnaDebug {}
+contract EchidnaTest is EchidnaSetup, EchidnaHelper, EchidnaDebug {
+    function testTokenURIShouldNotRevert(uint256 tokenId) public {
+
+        uint256 count = tray.nextTokenId() - 1;
+        require(count > 0);
+
+        tokenId = (tokenId % count) + 1;
+
+        try tray.tokenURI(tokenId) returns (string memory uri) {
+            Debugger.log("did not revert");
+        } catch {
+            Debugger.log("reverted");
+            assert(false);
+        }
+    }
+}
