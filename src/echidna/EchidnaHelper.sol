06b39bf4c8f6489feb67c9d8284f173c8dafeddb
Rappie <jasper@rappie.com>
2023-03-19 10:52:34 +0100

    Helpers for mint and approve

--- src/echidna/EchidnaHelper.sol
+++ src/echidna/EchidnaHelper.sol
@@ -3,11 +3,26 @@
 import "./EchidnaSetup.sol";
 
 contract EchidnaHelper is EchidnaSetup {
-
     function buy(uint8 fromAccId, uint256 amount) public {
         address from = getAccountFromUint8(fromAccId);
 
         hevm.prank(from);
         tray.buy(amount);
+    }
+
+    function mint(uint8 toAccId, uint256 amount) public {
+        address to = getAccountFromUint8(toAccId);
+        note.mint(to, amount);
+    }
+
+    function approve(uint8 fromAccId, uint256 amount) public {
+        address from = getAccountFromUint8(fromAccId);
+        hevm.prank(from);
+        note.approve(address(tray), amount);
+    }
+
+    function mintAndApprove(uint8 fromAccId, uint256 amount) public {
+        mint(fromAccId, amount);
+        approve(fromAccId, amount);
     }
 }
